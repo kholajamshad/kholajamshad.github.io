67be1817a13f522b139ff5be4e2f5c3c97949cfa
kholajamshad <57991557+kholajamshad@users.noreply.github.com>
2020-10-20 23:29:11 +0500

    added great experiment and mayfly to videopage

--- videoscript.js
+++ videoscript.js
@@ -22,4 +22,34 @@ button1.addEventListener("click", () => {
 
 
 
+let button2=document.getElementById("vidbutton2");
+let vidintro2=document.getElementById("vidintro2");
+button2.addEventListener("click", () => {
 
+	if (button2.innerText==">"){
+		button2.innerHTML="v";
+		vidintro2.innerHTML="Description";
+	}
+
+	else{
+		button2.innerHTML=">";
+		vidintro2.innerHTML="A retelling of the Earth experiment from Hitchhiker's Guide to the Galaxy from the perspective of the mice participating in it";
+	}
+});
+
+
+
+let button3=document.getElementById("vidbutton3");
+let vidintro3=document.getElementById("vidintro3");
+button3.addEventListener("click", () => {
+
+	if (button3.innerText==">"){
+		button3.innerHTML="v";
+		vidintro3.innerHTML="Description";
+	}
+
+	else{
+		button3.innerHTML=">";
+		vidintro3.innerHTML="An artificial chimera's journey of constant change from its first to final breath";
+	}
+});
